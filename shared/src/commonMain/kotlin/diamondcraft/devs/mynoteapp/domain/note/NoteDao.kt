25d2915cba73481cbacf9a0e3280b07de95d8a78
Manuel Duarte  <carlosd99m@gmail.com>
2023-06-08 23:51:42 -0600

    wip: initial code

---
+++ shared/src/commonMain/kotlin/diamondcraft/devs/mynoteapp/domain/note/NoteDao.kt
@@ -0,0 +1,8 @@
+package diamondcraft.devs.mynoteapp.domain.note
+
+interface NoteDao {
+    suspend fun  insertNote(note:Note)
+    suspend fun getNoteById(id :Long) :Note?
+    suspend fun getAllNotes() :List<Note>
+    suspend fun deleteNote(id: Long)
+}
